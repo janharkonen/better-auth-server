df61e923da25a5ada4dbdaed54582255e0e897f5
Jan Härkönen <jan.harkonen@gmail.com>
2026-09-08 10:57:16 +0300

    add migrations

---
+++ better-auth_migrations/2026-09-08T07-56-08.542Z.sql
@@ -0,0 +1,5 @@
+create table "user" ("id" text not null primary key, "name" text not null, "email" text not null unique, "emailVerified" boolean not null, "image" text, "createdAt" timestamptz default CURRENT_TIMESTAMP not null, "updatedAt" timestamptz default CURRENT_TIMESTAMP not null);
+
+create table "account" ("id" text not null primary key, "accountId" text not null, "providerId" text not null, "userId" text not null references "user" ("id") on delete cascade, "accessToken" text, "refreshToken" text, "idToken" text, "accessTokenExpiresAt" timestamptz, "refreshTokenExpiresAt" timestamptz, "scope" text, "password" text, "createdAt" timestamptz default CURRENT_TIMESTAMP not null, "updatedAt" timestamptz not null);
+
+create index "account_userId_idx" on "account" ("userId");
